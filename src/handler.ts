7798b82008b24e79caf91ead8e49d886db4daed8
Arda Soytürk <33841775+ardasoyturk@users.noreply.github.com>
2021-12-13 21:04:31 +0300

    feat: add likes endpoint

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,3 @@
-const twitterAPIUrl = 'https://api.twitter.com/1.1/users/lookup.json'
-
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
@@ -17,15 +15,39 @@ export async function handleRequest(request: Request): Promise<Response> {
   if (!url.searchParams.get('username'))
     return respond({ message: 'No username given.' })
 
-  const response = await fetch(
-    `${twitterAPIUrl}?screen_name=${url.searchParams.get('username')}`,
-    {
-      headers: {
-        'content-type': 'application/json;charset=UTF-8',
-        Authorization: `Bearer ${TWITTER_BEARER_TOKEN}`,
+  let response: Response
+
+  if (url.searchParams.get('lookup') === 'likes') {
+    const count = parseInt(url.searchParams.get('count') || '20')
+    const since_id = url.searchParams.get('since_id') || null
+    const max_id = url.searchParams.get('max_id') || null
+    response = await fetch(
+      `https://api.twitter.com/1.1/favorites/list.json?screen_name=${url.searchParams.get(
+        'username',
+      )}&count=${count}${since_id ? `&since_id=${since_id}` : ''}${
+        max_id ? `&max_id=${max_id}` : ''
+      }`,
+      {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+          Authorization: `Bearer ${TWITTER_BEARER_TOKEN}`,
+        },
       },
-    },
-  )
+    )
+  } else {
+    response = await fetch(
+      `https://api.twitter.com/1.1/users/lookup.json?screen_name=${url.searchParams.get(
+        'username',
+      )}`,
+      {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+          Authorization: `Bearer ${TWITTER_BEARER_TOKEN}`,
+        },
+      },
+    )
+  }
+
   const data: Record<string, unknown> = await response.json()
   return respond(data)
 }
